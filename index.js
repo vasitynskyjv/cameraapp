6d78be49ad04a3c671d5dee3db4bb190e4a8e70a
vasitynskyjv <vasitynskyj.v@gmail.com>
2018-01-07 02:38:15 +0200

    fast frame

---
+++ index.js
@@ -0,0 +1,40 @@
+$(function(){
+    var startCameraAbove = function(){
+                CameraPreview.startCamera({
+                tapEnabled: false,
+                toBack: false,
+                previewDrag: false,
+                tapPhoto: false});
+                hideCamera();
+    };
+
+    var stopCamera = function(){
+                setTimeout(function(){
+                    CameraPreview.stopCamera()},
+                    3000);
+    };
+
+    var hideCamera = function(){
+                    setTimeout(function(){
+                        CameraPreview.hide()},
+                        100);
+                    setTimeout(function(){
+                        takePicture()},
+                              1000);
+        };
+
+    var takePicture = function(){
+               setTimeout(function() {CameraPreview.takePicture({
+               width : 640 ,
+               height : 640 ,
+               quality : 85 },
+                    function ( base64PictureData ){
+                            imageData = 'data:image/jpeg;base64,' + base64PictureData;
+                            document.getElementById('originalPicture').src = imageData;
+                            });
+                },1000);
+                stopCamera();
+                };
+
+    document.getElementById('capturePhoto').addEventListener('click', startCameraAbove, false);
+});
